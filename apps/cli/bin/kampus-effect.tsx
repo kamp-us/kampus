56c86137e423af5b58cc9cde14f2a686267db1aa
usirin <umutsirin1@gmail.com>
2025-12-24 15:20:58 -0800

    add kampus-effect bin with effect-cli

---
+++ apps/cli/bin/kampus-effect.tsx
@@ -0,0 +1,33 @@
+#!/usr/bin/env bun
+
+import {Command} from "@effect/cli";
+import {BunContext, BunRuntime} from "@effect/platform-bun";
+import {Console, Effect, Layer} from "effect";
+
+import {login} from "../commands/login";
+import {KampusStateStorage} from "../services/KampusStateStorage";
+
+// Define the top-level command
+const command = Command.make("kampus", {}, () =>
+	Effect.gen(function* () {
+		const store = yield* KampusStateStorage;
+
+		yield* store.setSessionToken("foobar");
+
+		const token = yield* store.getSessionToken();
+
+		yield* Console.log(`Session Token: ${token}`);
+	}),
+).pipe(Command.withSubcommands([login]));
+
+// Set up the CLI application
+const cli = Command.run(command, {
+	name: "kampus cli - entrypoint to your kampus applications",
+	version: "v0.0.0",
+});
+
+const CliLive = Layer.mergeAll(KampusStateStorage.Default, BunContext.layer);
+
+const program = cli(process.argv).pipe(Effect.provide(CliLive));
+
+BunRuntime.runMain(program);
